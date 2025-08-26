4a1e6c37ff1e3ca854aef96671b1615efe49c488
megz15 <meghraj.g16@gmail.com>
2025-08-26 19:31:16 +0530

    fix: freq bug, note switcher

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -472,7 +472,7 @@
                 <div>
                     <div class="flex items-center justify-between gap-2">
                         <div class="text-white">Frequency (Hz)</div>
-                        <input type="number" bind:value={currBaseFreq} class="w-20 bg-gray-50 border-2 text-black text-sm rounded-lg px-2 py-1"/>
+                        <input type="number" bind:value={currBaseFreq} on:change={() => freqObject = genSaptakFreq(shrutis, currBaseFreq)} class="w-20 bg-gray-50 border-2 text-black text-sm rounded-lg px-2 py-1"/>
                     </div>
                     <input type="range" min=20 max=1000 step=10 bind:value={currBaseFreq} on:change={() => freqObject = genSaptakFreq(shrutis, currBaseFreq)} class="w-full h-2 rounded-lg appearance-none cursor-pointer bg-gray-700">
                 </div>
@@ -692,14 +692,20 @@
             noteEditModal ? 'opacity-100' : 'opacity-0 pointer-events-none'
     }`}>
     <div class="flex justify-between items-center mb-2">
-        <div class="text-xl font-bold">🔧 Note Edit</div>
+        <div class="text-xl font-bold">🔧 Svara/Matra Edit</div>
         <button class="text-2xl" on:click={() => noteEditModal = false}>❌</button>
     </div>
 
     <div class="flex flex-col justify-between gap-1 opacity-{isPlaybackStopped ? 100 : 10} pointer-events-{isPlaybackStopped ? 'auto' : 'none'}">
         <div class="flex flex-col gap-1">
             {#each currentBandishSectionSvaras[noteModalNoteIndex] as svaras, i}
-                <div class="flex justify-between">
+                <div class="flex justify-between gap-1">
+                    <button class="text-lg text-white w-10 bg-orange-500 font-medium rounded-lg py-2.5 hover:scale-105 active:scale-90 hover:shadow-orange-500/50 hover:shadow-[0_0_20px_5px] transition-all duration-200 active:duration-50 active:shadow-orange-500/50 active:shadow-[0_0_20px_5px]" on:click={() => {
+                        if (!["S", "P"].includes(svaras[0])) {
+                            svaras[0] = svaras[0].toLowerCase() === svaras[0] ? svaras[0].toUpperCase() : svaras[0].toLowerCase()
+                        }
+                    }}>🔁</button>
+
                     <input bind:value={svaras[0]} maxlength=1 on:click={this.select} class="w-12 bg-gray-50 border-2 text-black text-sm rounded-lg p-2.5"/>
                     <!-- <input bind:value={svaras[1]} type="number" class="w-16 bg-gray-50 border-2 text-black text-sm rounded-lg p-2.5"/> -->
 
@@ -714,13 +720,13 @@
                             svaras[1]++
                         }}>+</button>
                     </div>
-
-                    <button class="text-lg text-white w-12 bg-red-500 font-medium rounded-lg py-2.5 hover:scale-105 active:scale-90 hover:shadow-red-500/50 hover:shadow-[0_0_20px_5px] transition-all duration-200 active:duration-50 active:shadow-red-500/50 active:shadow-[0_0_20px_5px]" on:click={() => {
+                    
+                    <button class="text-lg text-white w-10 bg-red-500 font-medium rounded-lg py-2.5 hover:scale-105 active:scale-90 hover:shadow-red-500/50 hover:shadow-[0_0_20px_5px] transition-all duration-200 active:duration-50 active:shadow-red-500/50 active:shadow-[0_0_20px_5px]" on:click={() => {
                         if (currentBandishSectionSvaras[noteModalNoteIndex].length > 1) {
                             currentBandishSectionSvaras[noteModalNoteIndex].splice(i, 1)
                             currentBandishSectionSvaras = currentBandishSectionSvaras
                         } else alert("Can't delete base note!")
-                    }}>🔪</button>
+                    }}>🗑️</button>
                 </div>
             {/each}
         </div>
